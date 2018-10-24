9d5435216270cbba79dc231a124bdcb58c50d8f8
Emerson Carbonaro <emersonscarbonaro@gmail.com>
2018-10-24 20:38:13 -0300

    teste paciente

--- Teste/testePaciente.py
+++ Teste/testePaciente.py
@@ -10,12 +10,12 @@ def testeListarPaciente():
 def testeCadastrarPaciente():
     url = url_basica + '/paciente/cadastrar'
     paciente = {
-        "username":'ozob',
+        "username":'emerson',
          "password":"0420",
-         'nome':'ozob',
-         'email':'emerson@gmail.com',
+         'nome':'Loki',
+         'email':'sid@gmail.com',
          'celular':'11955554662',
-         'dataNascimento':'01081998',
+         'dataNascimento':'1998-08-01',
          'sexo':"M",
          'cidade':'são paulo',
          'profissao':'programador',
@@ -35,7 +35,7 @@ def testeExcluirPaciente(username):
     dados = Req.api.get(url).json()
     return dados
 
-def testeAlterarPaciente(username_atual, username, nome, email, celular, tipo, dataNascimento, sexo, cidade, profissao, objetivo):
+def testeAlterarPaciente(username_atual, username, nome, email, celular, tipo, dataNascimento, sexo, cidade, profissao, objetivo, altura):
     url = url_basica + '/paciente/alterar-paciente'
     paciente = {
         "username_atual": username_atual,
@@ -49,6 +49,7 @@ def testeAlterarPaciente(username_atual, username, nome, email, celular, tipo, d
         "cidade": cidade,
         "profissao": profissao,
         "objetivo": objetivo,
+        'altura':altura
         
     }
     dados = Req.api.put(url, json=paciente).json()
@@ -56,9 +57,9 @@ def testeAlterarPaciente(username_atual, username, nome, email, celular, tipo, d
 
 def main():
     #print(testeListarPaciente())
-    print(testeCadastrarPaciente())
+    #print(testeCadastrarPaciente())
     #print(testePesquisarPaciente('ozob'))
     #print(testeExcluirPaciente("ozob"))
-    #print(testeAlterarPaciente('ozob', 'ozob', 'Emerson TKP', 'lala@gmail.com', '11955554662', 'P','01081998', 'm', 'são paulo', 'devs', 'ganhar massa', altura))
+    print(testeAlterarPaciente('emerson', 'emerson', 'emerson', 'sid@gmail.com', '11955554662', 'P','1998-08-01', 'm', 'são paulo', 'devs', 'ganhar massa', 1.7))
 
 main()
