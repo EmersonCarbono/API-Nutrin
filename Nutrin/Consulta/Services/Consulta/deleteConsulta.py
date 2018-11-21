75efddc03616a7f65fa0835a0aec1d52b214ef8c
gabiconde <gabiconde@users.noreply.github.com>
2018-11-21 09:13:39 -0200

    alteração deleteConsulta

--- Nutrin/Consulta/Services/Consulta/deleteConsulta.py
+++ Nutrin/Consulta/Services/Consulta/deleteConsulta.py
@@ -3,6 +3,10 @@ def deleteConsulta(id_consulta):
     status, consulta = readConsultaId(id_consulta, True)
     if status:
         from Nutrin import db
+        from Nutrin.Consulta.Services.Ocupado.readOcupado import readOcupado
+        ocupStatus, ocup = readOcupado(consulta.horario_id, True)
+        if ocupStatus:
+            db.session.delete(ocup)
         db.session.delete(consulta)
         db.session.commit()
         return True, "Periodo excluido"
